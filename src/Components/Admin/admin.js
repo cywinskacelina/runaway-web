b351b1d672656a93e95d2482c03ce4e71cc73bf4
cedricblaise <cblaise@bowdoin.edu>
2020-08-18 19:11:34 -0400

    Design for admin page

--- src/Components/Admin/admin.js
+++ src/Components/Admin/admin.js
@@ -75,7 +75,12 @@ class Admin extends React.Component {
 
   render() {
     return (
+<<<<<<< HEAD
       <div style={{height: "100%" }} className = "col-10">
+=======
+      <div style={{ position: "absolute", width: "100%", height: "100%" }}>
+        <NavBar pages={[""]}/>
+>>>>>>> Design for admin page
         <div style={{ marginTop: "100px", marginLeft: "150px" }}>
         <form  onSubmit={(e) => this.handleSubmit(e)}>
           <input
