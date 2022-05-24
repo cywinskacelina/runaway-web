7704e906d260c78aa93ee358f9d13a3de059e451
Celina Cywinska <20262734+cywinskacelina@users.noreply.github.com>
2022-05-24 14:21:58 -0500

    Update chatComponent.js

--- src/Components/Chat/chatComponent.js
+++ src/Components/Chat/chatComponent.js
@@ -16,7 +16,7 @@ class ChatComponent extends React.Component {
         this._onMessageUpdate = this._onMessageUpdate.bind(this);
     }
 
-    //Send the server the room number to join.
+    //Send the server the room number to join and connect socket.
     socket_joinRoom(room) {
         this.state.socket.emit("joinRoom", room);
         this.state.socket.emit("volunteerJoined", "joined");
@@ -25,11 +25,10 @@ class ChatComponent extends React.Component {
 
 
     componentDidMount() {
-        
-        console.log(this.props.props[0]);
-        this.socket_joinRoom(parseInt(this.props.props[0],10))
+        this.socket_joinRoom(parseInt(this.props.props[0],10)) //join room.
     }// eslint-disable-line react-hooks/exhaustive-deps
 
+    //Append new message to the message state.
     _onMessageUpdate(message){
         this.setState({messages:[...this.state.messages, {
             author: 'them',
@@ -38,6 +37,8 @@ class ChatComponent extends React.Component {
         }]}
         );
     }
+    
+    //Send new message to the server. 
     _onMessageWasSent(message) {
         this.setState({
             messages: [...this.state.messages, message]
@@ -48,11 +49,14 @@ class ChatComponent extends React.Component {
 
 
     render() {
-        this.state.socket.on = this.state.socket.on.bind(this);
+        this.state.socket.on = this.state.socket.on.bind(this); //Binding the keyword "this" to this object.
+        
+        //When new message has been recieved
         this.state.socket.on("updateMessage", function (message) {
             console.log("message recieved");
             this._onMessageUpdate(message);
         })
+        
         return (
             <div className ="chatComponent" style={{ "z-index": this.props.props[1],"height":"100%"}}>
                 <Launcher
